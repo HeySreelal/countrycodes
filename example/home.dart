f7102270cedf3d01d049e8f2ab4d1756c2e939b1
Sreelal TS <heysreelal@gmail.com>
2022-04-06 20:49:59 +0530

    Coock some Example 🧑🏻‍🍳

---
+++ example/home.dart
@@ -0,0 +1,25 @@
+import 'package:countrycodes/src/country_field.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Country Codes'),
+      ),
+      body: Center(
+        child: CountryField(
+          onSelect: (c) {},
+        ),
+      ),
+    );
+  }
+}
